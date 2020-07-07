7cffc2f027f89e09186b2b265c53ea94df523646
Aline <aline_sinclaire@hotmail.fr>
2020-07-07 14:20:37 +0200

    started quest 20 with 5 fields

--- front/src/SignUp.js
+++ front/src/SignUp.js
@@ -4,12 +4,20 @@ class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: "",
+      lastname: "",
       email: "",
+      password: "",
+      passwordconf: "",
     };
   }
 
   handleChange = (event) => {
-    this.setState({ email: event.target.value });
+    console.log(this.state)
+    this.setState({ 
+      email: event.target.value,
+      lastname: event.target.value
+     });
     console.log(this.state.email);
   };
 
@@ -18,12 +26,38 @@ class SignUp extends React.Component {
       <div className="mx-auto mt-4 mb-4" style={{ width: "287px" }}>
         <h1>e-mail : {this.state.email}</h1>
 
-        <input
-          id="input"
-          placeholder="enter your email"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
+        <form>
+          <input
+            id="name"
+            placeholder="enter your name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input
+            id="lastname"
+            placeholder="enter your lastname"
+            value={this.state.lastname}
+            onChange={this.handleChange}
+          />
+          <input
+            id="email"
+            placeholder="enter your email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            id="password"
+            placeholder="enter your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <input
+            id="passwordconf"
+            placeholder="enter your password confirmation"
+            value={this.state.passwordconf}
+            onChange={this.handleChange}
+          />
+        </form>
       </div>
     );
   }
