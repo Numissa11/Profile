d6664b5ec60390a4b9f6e2b95fbe2bb0036eac95
Aline <aline_sinclaire@hotmail.fr>
2020-07-13 15:52:28 +0200

    back file: the connexion is made between the back and the front

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -2,30 +2,18 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../../sql/helpers/db');
 
-// http://localhost:5000/auth/signup
-router.post('/signup', function(req, res, next) {
-        const userData = req.body;
-        connection.query('INSERT INTO users SET ?', userData, (err, results) => {
-            if (err) {
-                return res.status(500).json({
-                  error: err.message,
-                  sql: err.sql,
-                });
-              }
-              return connection.query('SELECT * FROM users WHERE id = ?', results.insertId, (err2, records) => {
-                if (err2) {
-                  return res.status(500).json({
-                    error: err2.message,
-                    sql: err2.sql,
-                  });
-                }
-                const insertedUser = records[0];
-                return res
-                  .status(201)
-                  .json(insertedUser);
-              });
-            });
-        });
-
+// http://localhost:3000/auth/signup
+router.post('/signup', function (req, res, next) {
+      const formData = req.body;
+    
+      return connection.query(
+        'INSERT INTO users SET ?',
+        [formData],
+        (err, results) => {
+          if (err) res.status(500).json({ flash: err.message });
+          else res.status(200).json({ flash: 'User has been signed up!' });
+        },
+      );
+    });
 
 module.exports = router;
